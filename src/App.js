b3e503090944faa2b0165ce932115f27d15b3136
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-09 12:15:16 -0700

    Working on Transitions in Landing Page

--- src/App.js
+++ src/App.js
@@ -2,35 +2,58 @@ import React, { useState } from "react";
 import "./App.css";
 import "./PhoneView.css";
 import useInterval from "./useInterval";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
   const [introMsg, setIntroMsg] = useState(true);
+  const [secondMsg, setSecondMsg] = useState(false);
+
   useInterval(() => {
-    setIntroMsg(false);
+    if (introMsg) {
+      setIntroMsg(false);
+      setSecondMsg(true);
+    }
   }, 2000);
   return (
     <div className="App">
-      {!introMsg && (
+      <CSSTransition
+        in={!introMsg}
+        timeout={300}
+        classNames="title-intro"
+        unmountOnExit
+      >
         <React.Fragment>
           <div className="title">
             Hello. Welcome to Kirby Campbell's Home Page.
           </div>
-          <div className="main-container ">
-            <div className="inner-container ">
-              <div className="column-container animated slideInDown">
-                <div className="section animated slideInLeft">Resume</div>
-                <div className="section animated slideInRight">Portfolio</div>
-              </div>
-              <div className="column-container animated slideInUp  ">
-                <div className="section animated rotateInDownLeft">Blog</div>
-                <div className="section animated rotateInDownRight">Bio</div>
-              </div>
+        </React.Fragment>
+      </CSSTransition>
+      <CSSTransition
+        in={!introMsg}
+        timeout={300}
+        classNames="main-container"
+        unmountOnExit
+      >
+        <div className="main-container ">
+          <div className="inner-container ">
+            <div className="column-container animated slideInDown">
+              <div className="section animated slideInLeft">Resume</div>
+              <div className="section animated slideInRight">Portfolio</div>
+            </div>
+            <div className="column-container animated slideInUp  ">
+              <div className="section animated rotateInDownLeft">Blog</div>
+              <div className="section animated rotateInDownRight">Bio</div>
             </div>
           </div>
-          <div className="footer">Footer Butter</div>
-        </React.Fragment>
-      )}{" "}
-      {introMsg && (
+        </div>
+        {/* <div className="footer">Footer Butter</div> */}
+      </CSSTransition>
+      <CSSTransition
+        in={introMsg}
+        timeout={300}
+        classNames="title-intro"
+        unmountOnExit
+      >
         <React.Fragment>
           <div className="title-intro">
             Hello. Welcome to Kirby Campbell's Home Page.
@@ -38,7 +61,7 @@ function App() {
           <div className="main-container " />
           <div className="footer">Footer Butter</div>
         </React.Fragment>
-      )}
+      </CSSTransition>
     </div>
   );
 }
