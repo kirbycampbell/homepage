1844c9a78c32d3f7f994eda2d0ad137c04096249
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-08 23:23:01 -0700

    Adding css to HomePage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./PhoneView.css";
+import useInterval from "./useInterval";
 
 function App() {
+  const [introMsg, setIntroMsg] = useState(true);
+  useInterval(() => {
+    setIntroMsg(false);
+  }, 2000);
   return (
     <div className="App">
-      <div className="title">Hello. Welcome to Kirby Campbell's Home Page.</div>
-      <div className="main-container">
-        <div className="inner-container">
-          <div className="column-container">
-            <div className="section">Resume</div>
-            <div className="section">Portfolio</div>
+      {!introMsg && (
+        <React.Fragment>
+          <div className="title">
+            Hello. Welcome to Kirby Campbell's Home Page.
           </div>
-          <div className="column-container">
-            <div className="section">Blog</div>
-            <div className="section">Bio</div>
+          <div className="main-container ">
+            <div className="inner-container ">
+              <div className="column-container animated slideInDown">
+                <div className="section animated slideInLeft">Resume</div>
+                <div className="section animated slideInRight">Portfolio</div>
+              </div>
+              <div className="column-container animated slideInUp  ">
+                <div className="section animated rotateInDownLeft">Blog</div>
+                <div className="section animated rotateInDownRight">Bio</div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+          <div className="footer">Footer Butter</div>
+        </React.Fragment>
+      )}{" "}
+      {introMsg && (
+        <React.Fragment>
+          <div className="title--intro">
+            Hello. Welcome to Kirby Campbell's Home Page.
+          </div>
+          <div className="main-container " />
+          <div className="footer">Footer Butter</div>
+        </React.Fragment>
+      )}
     </div>
   );
 }
