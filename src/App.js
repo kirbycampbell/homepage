cf9f868de40a3fe74b53cc1261cebb2d694cba06
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-08 22:10:47 -0700

    Adding Boxes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ function App() {
       <div className="title">Hello. Welcome to Kirby Campbell's Home Page.</div>
       <div className="main-container">
         <div className="inner-container">
-          <div className="section">Resume</div>
-          <div className="section">Portfolio</div>
+          <div className="column-container">
+            <div className="section">Resume</div>
+            <div className="section">Portfolio</div>
+          </div>
+          <div className="column-container">
+            <div className="section">Blog</div>
+            <div className="section">Bio</div>
+          </div>
         </div>
       </div>
     </div>
