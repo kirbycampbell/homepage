36c4797a88abdbb2a4a3137b726feeb8e65ecfe6
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-20 22:04:48 -0700

    Adding Class Constructor Example for Portfolio Section

--- src/Containers/Portfolio.js
+++ src/Containers/Portfolio.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { projects } from "../kData/portfolioData";
 
 const Portfolio = () => {
-  return <div>portfolio</div>;
+  return (
+    <div className="portfolio-cont">
+      {projects.map(project => {
+        return (
+          <div>
+            <h1>{project.name}</h1>
+            <h2>{project.website}</h2>
+            <h2> {project.framework}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Portfolio;
---
+++ src/kData/portfolioData.js
@@ -0,0 +1,57 @@
+class Project {
+  constructor(name, website, framework, backend, info) {
+    this.name = name;
+    this.website = website;
+    this.framework = framework;
+    this.backend = backend;
+    this.info = info;
+  }
+  name() {
+    return this.name;
+  }
+  website() {
+    return this.name;
+  }
+  framework() {
+    return this.framework;
+  }
+  backend() {
+    return this.backend;
+  }
+  info() {
+    return this.info;
+  }
+}
+
+let resumeApp = new Project(
+  "ResumeApp",
+  "https://pure-waters-94797.herokuapp.com",
+  "React/CSS",
+  "Built-in",
+  "Fully Responsive Resume app."
+);
+let ticHackToe = new Project(
+  "Tic Hack Toe",
+  "https://damp-oasis-55552.herokuapp.com",
+  "React Hooks",
+  "LocalStorage",
+  "Example of Javascript ai. Expanded version of Tic Tac Toe with a 4x4 Board, Previous Game Access, Move Order per game, & hard to beat ai."
+);
+let drinkManager = new Project(
+  "Drink Manager",
+  "https://the-one-who-works.herokuapp.com/",
+  "React/Redux",
+  "Rails API",
+  "Final Project for Flatiron FullStack Bootcamp."
+);
+let blogCreator = new Project(
+  "Blog Creator",
+  "https://siq-blog.herokuapp.com/",
+  "React & Javascript",
+  "Google Firestore",
+  "Abstract Javascript & React example. Meant to resemble Medium.com."
+);
+
+const projects = [resumeApp, ticHackToe, drinkManager, blogCreator];
+
+export { projects };
