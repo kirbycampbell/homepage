6fc6215933c2c9307d31cb8afb8588c3ba8d885c
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-23 01:06:33 -0700

    Adding Designs

--- src/Containers/Resume.js
+++ src/Containers/Resume.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./Resume.css";
 
 const Resume = () => {
   return (
     <div>
-      <div>Resume App Link</div>
-      <div>Resume PDF Link</div>
+      <div className="resume-container">
+        <div className="resume-app-cont">
+          <div className="resume-pdf-link">Print</div>
+          <div className="resume-app-link">Interactive</div>
+          <img
+            src="https://i.imgur.com/DOlkeVw.jpg"
+            alt="resume"
+            className="resume-pic"
+          />
+        </div>
+      </div>
     </div>
   );
 };
