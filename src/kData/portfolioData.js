a1145efc09858786521d0eda44f4b8b5cfe473a3
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-24 19:59:26 -0700

    Adding Journal App

--- src/kData/portfolioData.js
+++ src/kData/portfolioData.js
@@ -36,7 +36,7 @@ let resumeApp = new Project(
   "React/CSS",
   "Built-in",
   "Fully Responsive Resume app.",
-  "https://github.com/kirbycampbell/kirby-resume",
+  "https://github.com/kirbycampbell/kirby-resume/tree/master/src",
   "https://i.imgur.com/lxfHGh9.jpg"
 );
 let ticHackToe = new Project(
@@ -45,7 +45,7 @@ let ticHackToe = new Project(
   "React Hooks",
   "LocalStorage",
   "0, 1, & 2 Player games of 4x4 Tic Tac Toe built with challenging javascript ai!",
-  "https://github.com/kirbycampbell/hikhakjoe",
+  "https://github.com/kirbycampbell/hikhakjoe/tree/master/src",
   "https://i.imgur.com/1tTko6L.jpg"
 );
 let drinkManager = new Project(
@@ -54,7 +54,7 @@ let drinkManager = new Project(
   "React/Redux",
   "Rails API",
   "Final Project for Flatiron FullStack Bootcamp.",
-  "https://github.com/kirbycampbell/oceanx",
+  "https://github.com/kirbycampbell/oceanx/tree/master/src",
   "https://i.imgur.com/Y6h9V9h.jpg"
 );
 let blogCreator = new Project(
@@ -63,8 +63,38 @@ let blogCreator = new Project(
   "React & Javascript",
   "Google Firestore",
   "Abstract Javascript & React example. Meant to resemble Medium.com.",
-  "https://github.com/kirbycampbell/blog-site",
+  "https://github.com/kirbycampbell/blog-site/tree/master/src",
   "https://i.imgur.com/NmffLct.jpg"
+);
+
+let vintageStore = new Project(
+  "Vintage Store",
+  "https://master.d3pkegk0xsoqlf.amplifyapp.com/",
+  "React & Javascript",
+  "LocalStorage",
+  "Modern Web Store page, exhibiting css styles & modern functionality.",
+  "https://github.com/kirbycampbell/store-front/tree/master/src",
+  "https://i.imgur.com/IHyZx0d.jpg"
+);
+
+let chatApp = new Project(
+  "Instant Messager",
+  "https://master.d2lekjvl9w1h5q.amplifyapp.com/",
+  "React & GraphQL",
+  "MongoDB & AWS Amplify",
+  "Instant Subscription based Chat Application.",
+  "https://github.com/kirbycampbell/chatterson-chatapp/tree/master/src",
+  "https://i.imgur.com/nhWZSTd.jpg"
+);
+
+let journalApp = new Project(
+  "News Site Clone",
+  "https://vue-journal-app-20190524193634-hostingbucket-vue.s3-us-west-2.amazonaws.com/index.html#/",
+  "Vue js & CSS",
+  "AWS Amplify",
+  "News page clone from scratch (CFR Clone).",
+  "https://github.com/kirbycampbell/journalapp/tree/master/src",
+  "https://i.imgur.com/AGv9362.jpg"
 );
 
 const projects = [
@@ -72,9 +102,9 @@ const projects = [
   ticHackToe,
   drinkManager,
   blogCreator,
-  resumeApp,
-  ticHackToe,
-  drinkManager,
+  vintageStore,
+  chatApp,
+  journalApp,
   resumeApp
 ];
 
