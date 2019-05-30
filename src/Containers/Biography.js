73d263bbeb0029d7a44b4a81c57285dfb08cc219
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-29 21:49:46 -0700

    Tweaking Bio Page

--- src/Containers/Biography.js
+++ src/Containers/Biography.js
@@ -1,13 +1,68 @@
 import React from "react";
+import "./Biography.css";
 
 const Biography = () => {
   return (
     <div className="bio-outer">
-      <div>Title</div>
-      <div>Video of me saying hey</div>
-      <div>Story about my life aw yeaaaa</div>
-      <div>Super cool facts about me</div>
-      <div>A picture or 2</div>
+      <div className="vid-box">Video goes here</div>
+      <div className="text-box">
+        Hi! My name is Kirby Campbell. I'm a 29 year old from Louisiana, living
+        in Portland Oregon. I am super passionate about coding, technology,
+        music, health, and creativity. I love to finish projects, experiment
+        with new ideas, and push the boundaries on the bleeding edge.
+      </div>
+
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        My first forays into the tech/creative world began at 14, when I began
+        building my own PCs. I soon became highly skilled at Audio Production -
+        co-producing my groups first album - In Light.
+        http://www.offbeat.com/articles/givers-remain-in-light/
+      </div>
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        After touring the world with my band from 2011-2016, I continued
+        producing for other artists, but soon began to crave creating
+        Interactive technological experiences. This initially brought me to
+        learn how to program Virtual Reality Experiences for Oculus Rift using
+        Unity & C++.
+      </div>
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        I then took part in a nanodegree from Udacity for Virtual Reality
+        Design. But I was left craving something more tangeable to the every day
+        person, as VR felt like a niche thus far.
+      </div>
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        This turn of events led me to FlatIron's FullStack Bootcamp. I
+        absolutely loved learning Ruby, Javascript, SQL, and jQuery. Once I
+        landed in the land of React and Redux, I was absolutely hooked. This is
+        what I had been looking for.
+      </div>
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        I completed FlatIron and focused on further honing my Full Stack skills
+        over the 5 months, adding GraphQL, AWS, Hooks, Vue, and more to "very
+        comfortable" skills list.
+      </div>
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        Beyond these tech skills, I'm very experienced in audio Programs like
+        Pro Tools, Logic, and Ableton, as well as graphics programs like Maya
+        and Unity. I'm also quite experienced in Adobe Premiere Pro & Photoshop.
+        I'm flexible between Mac & PC having spent about 50% on each platform
+        over the past 10 years. I've learned a good bit of C++ from the
+        nanodegree and through college level Computer Science courses, and I am
+        also familiar with UNIX and LINUX.
+      </div>
+      <div className="pic-box">Picture area</div>
+      <div className="text-box">
+        I hope you feel more connected to me after reading this quick summary of
+        my creative life and I would love to chat if you are interested in
+        Hiring me or learning more! Contact info is below - Thanks!
+      </div>
+      <div>-Kirby</div>
     </div>
   );
 };
