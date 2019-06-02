14c0bbeb65aa47ea4f25ba1622eb8ca373579daa
kirbycampbell <jkirbycampbell@gmail.com>
2019-06-01 17:11:25 -0700

    Fixed Wordings

--- src/Containers/Biography.js
+++ src/Containers/Biography.js
@@ -15,10 +15,10 @@ const Biography = () => {
         />
       </div>
       <div className="text-box">
-        Hi! My name is Kirby Campbell. I'm a 29 year old from Louisiana, living
-        in Portland Oregon. I am super passionate about coding, technology,
-        music, health, and creativity. I love to finish projects, experiment
-        with new ideas, and push the boundaries on the bleeding edge.
+        Hi! My name is Kirby Campbell. I'm a 29 year old from Louisiana, now
+        living in Portland Oregon. I am super passionate about coding,
+        technology, music, health, and creativity. I love to finish projects,
+        experiment with new ideas, and push the boundaries on the cutting edge.
       </div>
 
       <div className="pic-box">
@@ -29,7 +29,7 @@ const Biography = () => {
         />
       </div>
       <div className="text-box">
-        My first forays into the tech/creative world began at 14, when I began
+        My first journey into the tech/creative world began at 14, when I began
         building my own PCs. I soon became highly skilled at Audio Production -
         co-producing my group's first album -{" "}
         <a
@@ -49,9 +49,9 @@ const Biography = () => {
         />
       </div>
       <div className="text-box">
-        After touring the world with my band from 2011-2016, I continued
-        producing for other artists, but soon began to crave creating
-        Interactive technological experiences. This initially brought me to
+        After touring the world with my band, GIVERS, from 2011-2016, I
+        continued producing for other artists, but soon began to crave creating
+        interactive technological experiences. This initially brought me to
         learn how to program Virtual Reality Experiences for Oculus Rift using
         Unity & C++.
       </div>
@@ -65,7 +65,7 @@ const Biography = () => {
       </div>
       <div className="text-box">
         I then took part in a nanodegree from Udacity for Virtual Reality
-        Design. But I was left craving something more tangeable to the every day
+        Design, but I was left craving something more tangeable to the every day
         person, as VR felt like a niche thus far.
       </div>
       <div className="pic-box">
@@ -79,7 +79,7 @@ const Biography = () => {
       <div className="text-box">
         This turn of events led me to FlatIron's FullStack Bootcamp. I
         absolutely loved learning Ruby, Javascript, SQL, and jQuery. Once I
-        landed in the land of React and Redux, I was absolutely hooked. This is
+        arrived in the land of React and Redux, I was totally hooked. This is
         what I had been looking for.
       </div>
       <div className="pic-box">
@@ -92,8 +92,8 @@ const Biography = () => {
       </div>
       <div className="text-box">
         I completed FlatIron and focused on further honing my Full Stack skills
-        over the 5 months, adding GraphQL, AWS, Hooks, Vue, and more to "very
-        comfortable" skills list.
+        over the next 5 months, adding GraphQL, AWS, React Hooks, Vue, and more
+        to my skills list.
       </div>
       <div className="pic-box">
         {" "}
@@ -104,13 +104,13 @@ const Biography = () => {
         />
       </div>
       <div className="text-box">
-        Beyond these tech skills, I'm very experienced in audio Programs like
-        Pro Tools, Logic, and Ableton, as well as graphics programs like Maya
-        and Unity. I'm also quite experienced in Adobe Premiere Pro & Photoshop.
-        I'm flexible between Mac & PC having spent about 50% on each platform
-        over the past 10 years. I've learned a good bit of C++ from the
-        nanodegree and through college level Computer Science courses, and I am
-        also familiar with UNIX and LINUX.
+        Beyond these tech skills, I'm very experienced in audio programs like
+        Pro Tools, Logic, and Ableton, as well as graphics programs like Maya,
+        Unity, and Blender. I'm also quite experienced in Adobe Premiere &
+        Photoshop. I've learned a good bit of C++ through college level Computer
+        Science courses as well as the nanodegree. Additionally, I am familiar
+        with UNIX and Linux. I'm incredibly driven to learn new frameworks and
+        skills, as I am willing to tackle any challenge.
       </div>
       <div className="pic-box">
         {" "}
@@ -123,7 +123,7 @@ const Biography = () => {
       <div className="text-box">
         I hope you feel more connected to me after reading this quick summary of
         my creative life and I would love to chat if you are interested in
-        Hiring me or learning more! Contact info is below - Thanks!
+        hiring me or learning more! Contact info is below - Thanks!
       </div>
       <div className="pic-box">
         {" "}
