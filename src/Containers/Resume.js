ac736cf35f979ca13c46879d81be7c81d69e8755
kirbycampbell <jkirbycampbell@gmail.com>
2019-06-07 22:56:44 -0700

    Connected Resume Projects to Domains - Clickable

--- src/Containers/Resume.js
+++ src/Containers/Resume.js
@@ -5,19 +5,6 @@ import DesktopView from "../Components/DesktopView/DesktopView";
 const Resume = () => {
   return (
     <div>
-      {/* <div className="resume-container">
-        <div className="resume-app-cont">
-          <div className="resume-link-sec">
-            <div className="btnres resume-pdf-link">Print</div>
-            <div className="btnres resume-app-link">Interactive</div>
-          </div>
-          <img
-            src="https://i.imgur.com/DOlkeVw.jpg"
-            alt="resume"
-            className="resume-pic"
-          />
-        </div>
-      </div> */}
       <DesktopView />
     </div>
   );
